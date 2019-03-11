56985fd988609ffab4b1423fa9ba8dfab02ca390
Susie Jentoft <coo@sl-sas-compute-t1.ssb.no>
2019-03-11 11:19:07 +0100

    bug fixes

--- R/Convert_table.R
+++ R/Convert_table.R
@@ -7,13 +7,13 @@
 #'
 #' @return A dataata frame
 ConvertTable <- function(klass_data, code = "code", parentcode = "parentCode", level= "level"){
-    levelset = max(klass_data[,level])
+    levelset  <- as.numeric(max(klass_data[,level]))
     uni <- unique(klass_data[klass_data[, level]  == levelset, code])
     dx <- matrix(NA, length(uni), levelset)
     dx <- as.data.frame(dx)
     dx[, levelset] <- as.character(uni)
     for (i in 1:(levelset-1)){
-      m <- match(dx[,levelset - i + 1], dt[, code])
+      m <- match(dx[,levelset - i + 1], klass_data[, code])
       dx[, levelset-i] <- as.character(klass_data[, parentcode][m])
     }
     names(dx) <- paste("level", 1:levelset, sep="")
