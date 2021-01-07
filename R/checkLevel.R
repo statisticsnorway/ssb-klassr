d61c7d2ea7e51de0236383165b26c325e72fa658
Jentoft <coo@ssb.no>
2021-01-07 11:16:01 +0100

    Bug fix for classifications with non-numeric codes.

--- R/checkLevel.R
+++ R/checkLevel.R
@@ -1,33 +1,41 @@
-
-#' checking which level input data is
+#' Check which level input data/vector is
+#' 
+#' The input level is calculated as the mode (most common) heirarcichal 
+#' level in the input vector.
+#' 
 #' @param x - vector of character
 #' @param klass_data - - the right formatting to the classification levels
-#' @return The heirachical level of the input data is returned.
+#' @return The heiracichal level of the input data is returned. 
 #' @export
 #' @examples
 #' data(klassdata)
 #' 
 #' sn <- GetKlass(klass = "6", date = "2007-01-01")
 #' levelCheck(x = klassdata$nace5, klass_data = sn)
-#'
-#' sn <- GetKlass(klass = "7", date = "2007-01-01")
-#' levelCheck(x = klassdata$occupation, klass_data = sn)
 
 levelCheck <- function(x, klass_data){
-
-  tab <- data.frame(table(nchar(tm::removePunctuation(unlist(x)))))
-  t <- which.max(tab$Freq)
-
-  nVar <- tab[t,]$Var1
-  nVar <- nVar[!is.na(nVar)]
-
-  input_level <- NA
-  for (i in 1:length(unique(klass_data$level))){
-    if(nVar==nchar(tm::removePunctuation(unlist(klass_data[klass_data$level==i,]$code[1])))){
-      input_level <- i
+  # Check if only numbers are included. If yes, testing without formatting requirements
+  if(!all(grepl("^[A-Za-z_-]+$", x))){
+    tab <- data.frame(table(nchar(tm::removePunctuation(unlist(x)))))
+    t <- which.max(tab$Freq)
+    
+    nVar <- tab[t,]$Var1
+    nVar <- nVar[!is.na(nVar)]
+    
+    input_level <- NA
+    for (i in 1:length(unique(klass_data$level))){
+      if(nVar==nchar(tm::removePunctuation(unlist(klass_data[klass_data$level==i,]$code[1])))){
+        input_level <- i
+      }
     }
+    # if letters included, then vector is matched on codes
+  } else {
+    m <- match(x, klass_data$code)
+    tab <- table(klass_data[m, ]$level)
+    input_level <- as.numeric(names(tab)[which.max(tab)])
   }
   if(is.na(input_level))
-    stop("Cannot find a level")
+    stop("Cannot find an input level, please check the input vector")
   return (input_level)
 }
+
