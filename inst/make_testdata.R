ef67f025ec63c88df43adfb600340d597b62c353
Jentoft <coo@ssb.no>
2024-10-29 16:39:14 +0100

    Added test data and moved code for creation.

---
+++ inst/make_testdata.R
@@ -0,0 +1,39 @@
+# Packages ----------------------------------------------------------------
+
+# Making fixtures ---------------------------------------------------------
+
+## klass_131_graph.R ------------------------------------------------------
+library(klassR)
+klass_131_graph <- KlassGraph(classification = 131, date = "2024-10-29")
+
+save(klass_131_graph,
+  file ="./data/klass_131_graph.RData"
+)
+
+## klass_131_2020_graph.R -------------------------------------------------
+
+klass_131_2020_graph <- KlassGraph(
+  classification = 131,
+  date = "2020-01-01"
+)
+
+save(
+  klass_131_2020_graph,
+  file = "data/klass_131_2020_graph.RData"
+)
+
+## klass_131_1964_graph.R -------------------------------------------------
+
+## Endringen i kommunestruktur i 1964 er det enkeltåret som både har flest
+## splittelser og sammenslåinger av kommuner.
+
+klass_131_1964_graph <- KlassGraph(
+  classification = 131,
+  date = "1964-01-01"
+)
+
+save(
+  klass_131_1964_graph,
+  file ="data/klass_131_1964_graph.RData"
+)
+
