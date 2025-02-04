6189d3f4cb021a8c71083c20385017169fed7943
Jentoft <coo@ssb.no>
2025-02-04 10:02:19 +0100

    Swapped anonymous function so not reliant on R 4.1

--- R/UpdateKlass-graph-navigate.R
+++ R/UpdateKlass-graph-navigate.R
@@ -139,7 +139,7 @@ is_combined <- function(graph, node, compare_node = NULL) {
   } else {
     paths <- igraph::all_simple_paths(graph, node, start_nodes, mode = "in")
 
-    return(!all(vapply(paths, \(path) compare_node %in% path, logical(1))))
+    return(!all(vapply(paths, function(path) compare_node %in% path, logical(1))))
   }
 }
 
